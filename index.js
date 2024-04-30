bf72e084155e2378aeb60a3646ab2c2669864eee
Somlói Tamás <tamas.somloi@gmail.com>
2024-04-30 08:40:41 +0200

    mysql vercel test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
+const connection = require('./src/db')
 
 const port = process.env.PORT ?? 8008;
 
@@ -9,9 +10,27 @@ const myData = {
     age: 33
 }
 
+// For parsing application/json
+app.use(express.json());
+ 
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res, next)=> {
     res.send(myData)
-});
+})
+
+app.get("/users", (req, res, next)=>{
+    try {
+        connection.query("SELECT * FROM users LIMIT 5", (error, results, fields)=>{
+            if (error) throw error;
+            //console.log(fields);
+            res.send(results);
+        })
+    } catch (err) {
+        res.send("Hiba a kérés során")
+    }
+})
 
 app.listen(port, ()=> {
     console.log(`Server listening on ${port} port.`)
