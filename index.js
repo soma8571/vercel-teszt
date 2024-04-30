ec126786f55c44dcdb25e4d5a042407427a6ba00
Somlói Tamás <tamas.somloi@gmail.com>
2024-04-30 12:42:42 +0200

    tényleges mysql kérés ellenőrzése

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res, next)=> {
 })
 
 app.get("/users", (req, res, next)=>{
-    /* try {
+    try {
         connection.query("SELECT * FROM users LIMIT 5", (error, results, fields)=>{
             if (error) throw error;
             //console.log(fields);
@@ -30,8 +30,7 @@ app.get("/users", (req, res, next)=>{
         })
     } catch (err) {
         res.send("Hiba a kérés során")
-    } */
-    res.send(process.env.DB_HOST ?? "nincs db host")
+    }
 })
 
 app.listen(port, ()=> {
